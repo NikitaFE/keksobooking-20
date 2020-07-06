0ec86511b7320d4c7ff0b25d9e93ec1c0f93cb1e
NikitaFE <nikromongerredd@gmail.com>
2020-07-06 16:29:32 +0300

    fix some problems with functions and variables on brunch module3-task3

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 //  module3-task2
 //  mocks
-var titles = [
+var TITLES = [
   'Уютное гнездышко для молодоженов',
   'Маленькая квартирка рядом с парком',
   'Небольшая лавочка в парке',
@@ -12,13 +12,13 @@ var titles = [
   'Чёткая хата',
   'Стандартная квартира в центре'
 ];
-var xCoordinates = [500, 650, 340, 420, 550, 210, 170, 30];
-var yCoordinates = [500, 650, 340, 420, 550, 210, 170, 30];
-var roomPrices = [42000, 32000, 15000, 23300, 28000, 33500, 44400, 15700];
-var roomTypes = ['palace', 'flat', 'house', 'bungalo'];
-var roomTime = ['12:00', '13:00', '14:00'];
-var roomFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var roomDescriptions = [
+var X_COORDINATES = [500, 650, 340, 420, 550, 210, 170, 30];
+var Y_COORDINATES = [500, 650, 340, 420, 550, 210, 170, 30];
+var ROOM_PRICES = [42000, 32000, 15000, 23300, 28000, 33500, 44400, 15700];
+var ROOM_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var ROOM_TIME = ['12:00', '13:00', '14:00'];
+var ROOM_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var ROOM_DESCRIPTIONS = [
   'Великолепный таун-хауз в центре Токио. Подходит как туристам, так и бизнесменам. Дом полностью укомплектован и имеет свежий ремонт.',
   'Маленькая чистая квратира на краю парка. Без интернета, регистрации и СМС.',
   'Великолепная лавочка прямо в центре парка. Подходит для всех кто любит спать на свежем воздухе.',
@@ -28,11 +28,27 @@ var roomDescriptions = [
   'Тут красиво, светло и уютно. Есть где разместиться компании из 5 человек. Кофе и печеньки бесплатно.',
   'Квартира на первом этаже. Соседи тихие. Для всех, кто терпеть не может шума и суеты.'
 ];
-var roomPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var ROOM_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var TypeValues = {
+  'flat': 'Квартира',
+  'bungalo': 'Бунгало',
+  'house': 'Дом',
+  'palace': 'Дворец'
+};
 
 //  utils
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max + 1 - min) + min);
+};
+
+var generateNumbersArray = function (firstNum, lastNum) {
+  var numbersArray = [];
+
+  for (var i = firstNum; i <= lastNum; i++) {
+    numbersArray.push(i);
+  }
+
+  return numbersArray;
 };
 
 //  1. Напишите функцию для создания массива из 8 сгенерированных JS-объектов.
@@ -43,6 +59,7 @@ var MAP_X_MAX = MAIN_MAP_WIDTH - MAP_X_MIN;
 var MAP_Y_MIN = 130;
 var MAP_Y_MAX = 630;
 var AD_QUANTITY = 8;
+var IMAGES_NUMBERS = generateNumbersArray(1, 8);
 
 //  temp
 var ROOMS_MIN = 1;
@@ -62,23 +79,23 @@ var getFewItems = function (array) {
 };
 // temp
 
-var createAdvertisement = function (imageNumber, title, xCoordinate, yCoordinate, price, description) {
+var createAdvertisement = function (index) {
   var advertisement = {
     'author': {
-      'avatar': 'img/avatars/user0' + imageNumber + '.png'
+      'avatar': 'img/avatars/user0' + IMAGES_NUMBERS[index] + '.png'
     },
     'offer': {
-      'title': title,
-      'address': xCoordinate + ', ' + yCoordinate,
-      'price': price,
-      'type': roomTypes[getRandomNumber(0, roomTypes.length - 1)],
+      'title': TITLES[index],
+      'address': X_COORDINATES[index] + ', ' + Y_COORDINATES[index],
+      'price': ROOM_PRICES[index],
+      'type': ROOM_TYPES[getRandomNumber(0, ROOM_TYPES.length - 1)],
       'rooms': getRandomNumber(ROOMS_MIN, ROOMS_MAX),
       'guests': getRandomNumber(GUESTS_MIN, GUESTS_MAX),
-      'checkin': roomTime[getRandomNumber(0, roomTime.length - 1)],
-      'checkout': roomTime[getRandomNumber(0, roomTime.length - 1)],
-      'features': getFewItems(roomFeatures),
-      'description': description,
-      'photos': getFewItems(roomPhotos)
+      'checkin': ROOM_TIME[getRandomNumber(0, ROOM_TIME.length - 1)],
+      'checkout': ROOM_TIME[getRandomNumber(0, ROOM_TIME.length - 1)],
+      'features': getFewItems(ROOM_FEATURES),
+      'description': ROOM_DESCRIPTIONS[index],
+      'photos': getFewItems(ROOM_PHOTOS)
     },
     'location': {
       'x': getRandomNumber(MAP_X_MIN, MAP_X_MAX),
@@ -93,7 +110,7 @@ var createAdvertisements = function (num) {
   var advertisements = [];
 
   for (var i = 0; i < num; i++) {
-    advertisements.push(createAdvertisement(i + 1, titles[i], xCoordinates[i], yCoordinates[i], roomPrices[i], roomDescriptions[i]));
+    advertisements.push(createAdvertisement(i));
   }
 
   return advertisements;
@@ -140,26 +157,13 @@ renderPins(createAdvertisements(AD_QUANTITY));
 
 //  module3-task3
 //  1. На основе первого по порядку элемента из сгенерированного массива и шаблона #card создайте DOM-элемент объявления.
+var HOUSE_PHOTO_WIDTH = 45;
+var HOUSE_PHOTO_HEIGHT = 40;
+var HOUSE_PHOTO_ALT = 'Фотография жилья';
+
 var cardTemplate = document.querySelector('#card')
                             .content
                             .querySelector('.map__card');
-
-//  temp
-var translateType = function (type) {
-  switch (type) {
-    case 'flat':
-      return 'Квартира';
-    case 'bungalo':
-      return 'Бунгало';
-    case 'house':
-      return 'Дом';
-    case 'palace':
-      return 'Дворец';
-    default:
-      return 'Сарай';
-  }
-};
-//  temp
 
 var fillFeaturesList = function (features) {
   var featuresFragment = document.createDocumentFragment();
@@ -181,9 +185,9 @@ var fillPhotosList = function (photos) {
     var photoItem = document.createElement('img');
     photoItem.className = 'popup__photo';
     photoItem.src = item;
-    photoItem.width = 45;
-    photoItem.height = 40;
-    photoItem.alt = 'Фотография жилья';
+    photoItem.width = HOUSE_PHOTO_WIDTH;
+    photoItem.height = HOUSE_PHOTO_HEIGHT;
+    photoItem.alt = HOUSE_PHOTO_ALT;
     photosFragment.appendChild(photoItem);
   });
 
@@ -197,7 +201,7 @@ var createAdvertisementCard = function (data) {
   cardElement.querySelector('.popup__title').textContent = offer.title;
   cardElement.querySelector('.popup__text--address').textContent = offer.address;
   cardElement.querySelector('.popup__text--price').textContent = offer.price + '₽/ночь';
-  cardElement.querySelector('.popup__type').textContent = translateType(offer.type);
+  cardElement.querySelector('.popup__type').textContent = TypeValues[offer.type];
   cardElement.querySelector('.popup__text--capacity').textContent = offer.rooms + ' комнаты для ' + offer.guests + ' гостей';
   cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.checkin + ' , выезд до ' + offer.checkout;
   cardElement.querySelector('.popup__features').innerHTML = '';
